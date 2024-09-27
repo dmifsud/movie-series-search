18f79df5bb8e27fa9ae755c07702b966a7eb1157
David Mifsud <david.mifsud89@gmail.com>
2024-09-27 16:25:20 +0930

    feat: nav and search styling

---
+++ src/components/SearchMovie.tsx
@@ -0,0 +1,121 @@
+import { TitleSearchQuery } from '@api/models/omdb.schema';
+import useSearchMoviesStore from '@store/search-movie.store';
+import { useState, useRef, useCallback, useEffect } from 'react';
+
+function SearchIcon() {
+    return (
+        <svg
+            className="w-6 h-6 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+        >
+            <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+        </svg>
+    );
+}
+
+export type SearchMovieType = TitleSearchQuery['type'] | 'any';
+function SearchMovie({
+    search,
+    type: propType,
+}: {
+    search?: string;
+    type?: SearchMovieType;
+}) {
+    const { actions } = useSearchMoviesStore.getState();
+    const [movieType, setMovieType] = useState<SearchMovieType>(
+        propType ?? 'any'
+    );
+    const searchRef = useRef<HTMLInputElement>(null);
+
+    const searchMovieByTitle = useCallback(actions.searchMovieByTitle, []);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const searchValue = searchRef.current?.value || '';
+        searchMovieByTitle(searchValue, {}, true);
+    };
+
+    useEffect(() => {
+        const searchValue = searchRef.current?.value || '';
+        if (searchValue) {
+            let type;
+            if (movieType !== 'any') {
+                type = movieType;
+            }
+            searchMovieByTitle(searchValue, { type }, true);
+        }
+    }, [movieType]);
+
+    const onOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMovieType(e.target.value as SearchMovieType);
+    };
+
+    console.log('render search movie');
+
+    return (
+        <form onSubmit={handleSearch} className="flex justify-between">
+            <div>
+                <label
+                    htmlFor="default-search"
+                    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                    Search
+                </label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <SearchIcon />
+                    </div>
+                    <input
+                        type="search"
+                        id="default-search"
+                        defaultValue={search}
+                        ref={searchRef}
+                        className="block w-full py-4 px-[55px] text-xl bg-transparent text-white focus:ring-0 focus:border-transparent focus:ring-offset-0 focus:outline-none"
+                        placeholder="Search Movies..."
+                    />
+                </div>
+            </div>
+
+            {/* <MultiRangeSlider min={1950} max={2024} /> */}
+            <div className="flex gap-5">
+                {[
+                    ['any', 'Any'],
+                    ['movie', 'Movie'],
+                    ['series', 'Series'],
+                    ['episodes', 'Episodes', 'disabled'],
+                ].map(([key, name, disabled]) => (
+                    <div key={key} className="inline-flex items-center">
+                        <label
+                            className="relative flex items-center gap-2 cursor-pointer peer-disabled:cursor-default text-white"
+                            htmlFor={key}
+                        >
+                            <input
+                                name="inline-radio-group"
+                                type="radio"
+                                className="peer h-5 w-5 cursor-pointer disabled:cursor-default appearance-none rounded-full border-2 border-white checked:border-white transition-all disabled:border-primary-light disabled:opacity-50"
+                                id={key}
+                                value={key}
+                                onChange={onOptionChange}
+                                checked={movieType === key}
+                                disabled={!!disabled}
+                            />
+                            <span className="absolute left-[10px] bg-white w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 transform -translate-x-1/2 -translate-y-1/2 peer-disabled:opacity-50"></span>
+                            {name}
+                        </label>
+                    </div>
+                ))}
+            </div>
+        </form>
+    );
+}
+
+export default SearchMovie;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,16 +1,19 @@
-import { TitleSearchQuery } from '@api/models/omdb.schema'
-import useQueryParams from '@hooks/useQueryParams'
-import useGetMovieStore from '@store/get-movie.store'
-import useMovieWatchlistStore from '@store/movie-watchlist.store'
-import useSearchMoviesStore from '@store/search-movie.store'
-import { useCallback, useEffect, useRef, useState } from 'react'
-import { Link } from 'wouter'
+import { TitleSearchQuery } from '@api/models/omdb.schema';
+import SearchMovie, { SearchMovieType } from '@components/SearchMovie';
+import BookmarkHeart from '@components/ui/icons/BookmarkHeart';
+import BookmarkIcon from '@components/ui/icons/BookmarkIcon';
+import useQueryParams from '@hooks/useQueryParams';
+import useGetMovieStore from '@store/get-movie.store';
+import useMovieWatchlistStore from '@store/movie-watchlist.store';
+import useSearchMoviesStore from '@store/search-movie.store';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Link } from 'wouter';
 
 function TempMovie() {
-    const { data, loading, inWatchlist } = useGetMovieStore()
-    const { actions } = useMovieWatchlistStore.getState()
+    const { data, loading, inWatchlist } = useGetMovieStore();
+    const { actions } = useMovieWatchlistStore.getState();
 
-    console.log('render movie', data)
+    console.log('render movie', data);
 
     return (
         <div>
@@ -32,7 +35,7 @@ function TempMovie() {
                 </>
             )}
         </div>
-    )
+    );
 }
 
 function TempMovies() {
@@ -42,13 +45,13 @@ function TempMovies() {
         totalResults,
         actions: searchActions,
         canLoadMore,
-    } = useSearchMoviesStore()
-    const { actions } = useGetMovieStore.getState()
+    } = useSearchMoviesStore();
+    const { actions } = useGetMovieStore.getState();
 
-    const getMovieByImdbID = useCallback(actions.getMovieByImdbID, [])
-    const searchMore = useCallback(searchActions.searchMore, [])
+    const getMovieByImdbID = useCallback(actions.getMovieByImdbID, []);
+    const searchMore = useCallback(searchActions.searchMore, []);
 
-    console.log('render movies', { data, loading, totalResults })
+    console.log('render movies', { data, loading, totalResults });
     return (
         <div>
             {totalResults !== null && `${totalResults} RESULTS`}
@@ -70,163 +73,39 @@ function TempMovies() {
                 </button>
             )}
         </div>
-    )
+    );
 }
 
-type MovieType = TitleSearchQuery['type'] | 'any'
-function SearchMovie({
-    search,
-    type: propType,
-}: {
-    search?: string
-    type?: MovieType
-}) {
-    const { actions } = useSearchMoviesStore.getState()
-    const [movieType, setMovieType] = useState<MovieType>(propType ?? 'any')
-    const searchRef = useRef<HTMLInputElement>(null)
-
-    const searchMovieByTitle = useCallback(actions.searchMovieByTitle, [])
-
-    const handleSearch = (e: React.ChangeEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const searchValue = searchRef.current?.value || ''
-        searchMovieByTitle(searchValue, {}, true)
-    }
-
-    useEffect(() => {
-        const searchValue = searchRef.current?.value || ''
-        if (searchValue) {
-            let type
-            if (movieType !== 'any') {
-                type = movieType
-            }
-            searchMovieByTitle(searchValue, { type }, true)
-        }
-    }, [movieType])
-
-    const onOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setMovieType(e.target.value as MovieType)
-    }
-
-    console.log('render search movie')
-
+function FixedWatchlistLink() {
+    const { movies } = useMovieWatchlistStore();
     return (
-        <form onSubmit={handleSearch}>
-            <input
-                type="search"
-                placeholder="Search Movie"
-                defaultValue={search}
-                ref={searchRef}
-            />
-            {/* <MultiRangeSlider min={1950} max={2024} /> */}
-            <div className="flex">
-                <div className="flex items-center me-4">
-                    <input
-                        id="inline-radio"
-                        onChange={onOptionChange}
-                        checked={movieType === 'any'}
-                        type="radio"
-                        value="any"
-                        name="inline-radio-group"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                        htmlFor="inline-radio"
-                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Any
-                    </label>
-                </div>
-                <div className="flex items-center me-4">
-                    <input
-                        id="inline-2-radio"
-                        onChange={onOptionChange}
-                        checked={movieType === 'movie'}
-                        type="radio"
-                        value="movie"
-                        name="inline-radio-group"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                        htmlFor="inline-2-radio"
-                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Movies
-                    </label>
-                </div>
-                <div className="flex items-center me-4">
-                    <input
-                        id="inline-checked-radio"
-                        onChange={onOptionChange}
-                        checked={movieType === 'series'}
-                        type="radio"
-                        value="series"
-                        name="inline-radio-group"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                        htmlFor="inline-checked-radio"
-                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                        Series
-                    </label>
-                </div>
-                <div className="flex items-center">
-                    <input
-                        disabled
-                        id="inline-disabled-radio"
-                        checked={movieType === 'episode'}
-                        onChange={onOptionChange}
-                        type="radio"
-                        value="episode"
-                        name="inline-radio-group"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                        htmlFor="inline-disabled-radio"
-                        className="ms-2 text-sm font-medium text-gray-400 dark:text-gray-500"
-                    >
-                        Episodes
-                    </label>
-                </div>
-            </div>
-        </form>
-    )
+        <>
+            {movies.length > 0 && (
+                <Link
+                    to="/watchlist"
+                    title="Watchlist Page"
+                    className="fixed top-[-8px] right-10"
+                >
+                    <BookmarkHeart className="hover:fill-primary-light" />
+                </Link>
+            )}
+        </>
+    );
 }
 
 function SearchPage() {
-    const params = useQueryParams()
-    // const { actions } = useSearchMoviesStore.getState();
+    const params = useQueryParams();
 
-    // useEffect(() => {
-    //     const { s, ...rest } = params;
-    //     const optionalEntries = Object.entries(rest).filter(([key]) => ['y', 'type', 'page'].includes(key));
-    //     if (s) {
-
-    //         const query: TitleSearchQuery = {
-    //             s,
-    //             ...Object.fromEntries(optionalEntries)
-    //         };
-    //         console.log('call search with', query);
-    //         actions.searchMovie(query)
-    //     }
-    // }, [params, actions]);
-
-    console.log('render search')
+    console.log('render search');
 
     return (
         <div className="flex fixed w-full h-full flex-col">
-            <nav className="p-4 bg-primary text-white">
+            <nav className="py-6 px-16 bg-primary text-white">
                 <SearchMovie
                     search={params['s']}
-                    type={params['type'] as MovieType}
+                    type={params['type'] as SearchMovieType}
                 />
-                <Link
-                    to="/watchlist"
-                    className="mt-20 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
-                >
-                    Watchlist
-                </Link>
+                <FixedWatchlistLink />
             </nav>
             <section className="flex flex-grow justify-stretch overflow-hidden">
                 <div className="box-border w-[30%] min-w-[300px] max-w-[600px] border-r-2 border-r-secondary border-solid overflow-y-auto custom-scrollbar">
@@ -240,7 +119,7 @@ function SearchPage() {
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
-export default SearchPage
+export default SearchPage;
